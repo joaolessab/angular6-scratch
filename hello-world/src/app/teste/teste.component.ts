52343eac3718fc5b2bac90c0595481b96c181b44
João Vitor Lessa <joaovitorlessa@gmail.com>
2018-04-23 13:57:38 -0300

    Type of selectors

--- hello-world/src/app/teste/teste.component.ts
+++ hello-world/src/app/teste/teste.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-teste',
+  //selector: '.app-teste',
+  //selector: '[app-teste]'
   templateUrl: './teste.component.html',
   styleUrls: ['./teste.component.css']
 })
