9e3182c1d80fc211da0de77f5b2ce035f1b85546
João Vitor Lessa <joaovitorlessa@gmail.com>
2018-04-23 14:23:33 -0300

    Examples of Interpolations

--- binding/src/app/test/test.component.ts
+++ binding/src/app/test/test.component.ts
@@ -4,17 +4,27 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-test',
   //Template Inline
   template: `
-    <h2>
-      Welcome Wishas
-    </h2>
+    <h2>Welcome {{name}}</h2>
+    <h2>{{2+2}}</h2>
+    <h2>{{"Welcome "+ name}}</h2>
+    <h2>{{name.length}}</h2>
+    <h2>{{name.toUpperCase()}}</h2>
+    <h2>{{greetUser()}}</h2>
+    <h2>{{siteUrl}}</h2>
   `,
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
 
+  public name = "João";
+  public siteUrl = window.location.href;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  greetUser(){
+    return "Hello " + this.name;
   }
 
 }
