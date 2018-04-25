236be12377551f7f24503f1e9f6cade817b12e0d
João Vitor Lessa <joaovitorlessa@gmail.com>
2018-04-24 23:57:50 -0300

    ngSwitch

--- structure-directives/src/app/test/test.component.ts
+++ structure-directives/src/app/test/test.component.ts
@@ -13,12 +13,21 @@ import { Component, OnInit } from '@angular/core';
     <ng-template #elseBlock>
       <h2>Name is hidden</h2>
     </ng-template>
+
+    <!-- ngSwitch -->
+    <div [ngSwitch]="color">
+      <div *ngSwitchCase="'red'">You picked the red color</div>
+      <div *ngSwitchCase="'blue'">You picked the blue color</div>
+      <div *ngSwitchCase="'green'">You picked the green color</div>
+      <div *ngSwitchDefault>Pick again</div>
+    </div>
   `,
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
 
   public displayName = true;
+  public color = "blue";
   constructor() { }
 
   ngOnInit() {
