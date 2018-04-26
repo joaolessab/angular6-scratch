4dc6981736e2ebbb3a49ece65216d1ac0b2d1588
João Vitor Lessa <joaovitorlessa@gmail.com>
2018-04-25 23:16:53 -0300

    Sending data from Child do Parent Component

--- structure-directives/src/app/testing/testing.component.ts
+++ structure-directives/src/app/testing/testing.component.ts
@@ -1,22 +1,33 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-testing',
   template: `
     <h2>{{"Hello " + parentData}}</h2>
     <!--h2>{{"Hello " + name}}</h2-->
+
+    <!-- Sending Data -->
+    <button (click)="fireEvent()">Send Data</button>
   `,
   styleUrls: ['./testing.component.css']
 })
 export class TestingComponent implements OnInit {
 
-  //Getting parentData
+  //Getting data from Parent Component
   @Input() public parentData;
   //@Input('parentData') public name;
+
+  //Sending to Parent Component
+  @Output() public childEvent = new EventEmitter();
   
   constructor() { }
 
   ngOnInit() {
+  }
+
+  //Event created to emit
+  fireEvent(){
+    this.childEvent.emit("Hey Codevolution");  
   }
 
 }
