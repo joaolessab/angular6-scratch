4672b8a903eb909c416474eff41e675f8e409e89
João Vitor Lessa <joaovitorlessa@gmail.com>
2018-06-03 19:44:21 -0300

    Inline Style

--- routing-demo/src/app/department-list/department-list.component.ts
+++ routing-demo/src/app/department-list/department-list.component.ts
@@ -6,21 +6,22 @@ import { Router } from '@angular/router';
   template: `
     <h3>Department List</h3>
     <ul class="items">
-      <li (click)="onSelect(department)" *ngFor="let department of departments">
+      <li (click)="onSelect(department)" *ngFor="let department of departments" class="route-btn">
         <span class="badge">{{department.id}}</span> {{department.name}}
       </li>
     </ul>
   `,
-  styles: []
+  styles: ['.route-btn { background-color: #ffd7d7; border-radius: 3px; width: 90px; margin: 10px; padding: 10px}', 
+          '.route-btn:hover { cursor: pointer; background-color: red; color: white }']
 })
 export class DepartmentListComponent implements OnInit {
 
   departments = [
-    {"id": 1, "name": "Angular"},
-    {"id": 2, "name": "Node"},
-    {"id": 3, "name": "MongoDB"},
-    {"id": 4, "name": "Ruby"},
-    {"id": 5, "name": "Bootstrap"}
+    {'id': 1, 'name': 'Angular'},
+    {'id': 2, 'name': 'Node'},
+    {'id': 3, 'name': 'MongoDB'},
+    {'id': 4, 'name': 'Ruby'},
+    {'id': 5, 'name': 'Bootstrap'}
   ];
 
   constructor(private router: Router) { }
@@ -28,7 +29,7 @@ export class DepartmentListComponent implements OnInit {
   ngOnInit() {
   }
 
-  onSelect(department){
+  onSelect(department) {
     this.router.navigate(['/departments', department.id]);
   }
 }
