b06265ef320f6b6637eb978f9a73f2ac53b555d1
João Vitor Lessa <joaovitorlessa@gmail.com>
2018-07-10 20:46:15 -0300

    [Login App] - Basic Structure for Login

---
+++ login-app/src/app/auth.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() { }
+
+  getUserDetails(){
+    //Post these details to API Server and return user info if it's correct
+  }
+}
